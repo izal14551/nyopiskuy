0659b4a4a1a2d577a77c8683ee966238ccc694d5
izal14551 <erwin14551@gmail.com>
2025-08-26 12:26:16 +0700

    lokasi kampus

--- app/api/config/route.js
+++ app/api/config/route.js
@@ -4,8 +4,8 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  const lat = process.env.KEDAI_LAT ?? "-7.442048556027217";
-  const lng = process.env.KEDAI_LNG ?? "109.27106001450271";
+  const lat = process.env.KEDAI_LAT ?? "-7.43927617825922";
+  const lng = process.env.KEDAI_LNG ?? "109.26619712576532";
 
   return NextResponse.json(
     { lat: Number(lat), lng: Number(lng) },
